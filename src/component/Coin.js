925fdf1a013bceb99015c0d6d39a14e23bd0a235
Throughbit <karthikeyan@throughbit.com>
2024-05-31 17:30:52 +0530

    socket update

--- src/component/Coin.js
+++ src/component/Coin.js
@@ -103,8 +103,8 @@ const Coin = () => {
 
   const [activeUsers, setActiveUsers] = useState(0);
 
-  useEffect(() => {
-    const ws = new WebSocket("ws://hippie-pepe-be.onrender.com");
+  useEffect(async () => {
+    const ws = new WebSocket("ws://localhost:3000");
 
     ws.onmessage = (message) => {
       const data = JSON.parse(message.data);
