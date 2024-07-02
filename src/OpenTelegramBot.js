2243d25071fb8391c97d60c48b637d2e8ebed4ca
MuraliThroughbit <149476540+MuraliThroughbit@users.noreply.github.com>
2024-07-02 12:52:03 +0530

    Add files via upload

---
+++ src/OpenTelegramBot.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const OpenTelegramBot = ({ onClick }) => {
+  const botUsername = "degenTv"; // Replace with your bot's username
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    // Set a flag in session storage to indicate the link was clicked
+    sessionStorage.setItem("visitedTelegramBot", "true");
+    // Open Telegram bot in the same window
+    window.location.href = `https://t.me/${botUsername}`;
+  };
+
+  return (
+    <div>
+      <a href={`https://t.me/${botUsername}`} onClick={handleClick}>
+        Open Telegram Bot
+      </a>
+    </div>
+  );
+};
+
+export default OpenTelegramBot;
