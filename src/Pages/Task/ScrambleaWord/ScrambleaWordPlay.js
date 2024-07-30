84199e767478438949bdfaa80ff0a454d20a2104
muralirajCSE29 <murali@MuraliRaj.local>
2024-07-30 10:51:20 +0530

    scrambleUI

--- src/Pages/Task/ScrambleaWord/ScrambleaWordPlay.js
+++ src/Pages/Task/ScrambleaWord/ScrambleaWordPlay.js
@@ -3,30 +3,32 @@ import "./ScrambleaWordPlay.css";
 
 const ScrambleaWordPlay = () => {
   return (
-    <div>
-      <div className="ScrambleaWordPlay">
-        <h1 className="text-heading">WORD SCRAMBLE</h1>
-        <h3 className="scramble-count">YOU HAVE 1/5 SCRAMBLE</h3>
-      </div>
-      <div className="scramble-wordbox-container">
-        <div className="scramble-wordbox"></div>
-        <div className="scramble-wordbox"></div>
-        <div className="scramble-wordbox"></div>
-        <div className="scramble-wordbox"></div>
-        <div className="scramble-wordbox"></div>
-      </div>
+    <>
       <div>
-        <h2 className="hint-text">Hint</h2>
-        <h2 className="first-cryptocurrency">FIRST CRYPTOCURRENCY</h2>
+        <div className="ScrambleaWordPlay">
+          <h1 className="text-heading">WORD SCRAMBLE</h1>
+          <h3 className="scramble-count">YOU HAVE 1/5 SCRAMBLE</h3>
+        </div>
+        <div className="scramble-wordbox-container">
+          <div className="scramble-wordbox"></div>
+          <div className="scramble-wordbox"></div>
+          <div className="scramble-wordbox"></div>
+          <div className="scramble-wordbox"></div>
+          <div className="scramble-wordbox"></div>
+        </div>
+        <div>
+          <h2 className="hint-text">Hint</h2>
+          <h2 className="first-cryptocurrency">FIRST CRYPTOCURRENCY</h2>
+        </div>
+        <div className="text-center">
+          <input
+            className="input-text"
+            type="text"
+            placeholder="ENTER A VALID WORD"
+          ></input>
+        </div>
       </div>
-      <div>
-        <input
-          className="input-text"
-          type="text"
-          placeholder="ENTER A VALID WORD"
-        ></input>
-      </div>
-    </div>
+    </>
   );
 };
 
