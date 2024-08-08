8236702703d4d116f0d3f1152ba7b4819bc27560
sadham <sadhampar27@gmail.com>
2024-08-08 16:32:53 +0530

    doearn

--- src/Pages/DoEarn/DoEarn.jsx
+++ src/Pages/DoEarn/DoEarn.jsx
@@ -3,31 +3,84 @@ import './DoEarn.css';
 import DoEarnImg from '../../assets/images/doearn.png';
 import X from '../../assets/images/x.png';
 import Coin from '../../assets/images/coin.png';
+import earn from '../../assets/images/earn-txt.png';
+import twitter from '../../assets/images/twitter.svg';
+import logo from '../../assets/images/meme-logo.svg';
+import youtube from '../../assets/images/youtube.svg';
+import telegram from '../../assets/images/telegram.svg';
+import dobottom from '../../assets/images/stuff.gif';
 
 const DoEarn = () => {
   return (
-    <div>
-      <div style={{ marginTop: '80px' }}>
-        <img src={DoEarnImg} alt="" />
-      </div>
-
-      {/* Game and Task */}
-      <div
-        style={{ backgroundColor: 'rgba(33, 33, 33, 1)' }}
-        className="w-[90%] border rounded-sm mx-auto flex items-center justify-around text-white h-24 mt-4 totalpoints text-xs font-[10]"
-      >
-        {/* Todoiooooo */}
-        <img className="h-12" src={X} alt="" />
-        <div className="flex flex-col items-center justify-center w-full">
-          <p className="whitespace-nowrap">Join our X channel</p>
-          <div className="flex items-center justify-center">
-            <img className="h-12" src={Coin} alt="" />
-            <p className="mb-0 ml-2">+5,000</p>
+    <div className="info-img">
+    <div
+      className="menupointer stuff-body"
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        // justifyContent: "center",
+        marginTop:"7%",
+        flexDirection: "column",
+        pointerEvents: "all",
+      }}>
+        <div className='row'>
+          <div className='col-12'>
+          <div className='doearn-img'> 
+            <img className='stuff-gif' src={dobottom} />
+            <div className='earn-img'>
+              <img src={earn} />
+            </div>
+</div>
           </div>
         </div>
-        <button className="border py-1 px-6 rounded-md">Go</button>
+        <div className='row mt20 cheap-stuff' style={{width:"100%"}}>
+          <div className='col-2'>
+            <img src={twitter} style={{width:"100%"}} />
+            
+          </div>
+          <div className='col-7 stuff-text'>
+            <h4>Join our x Channel</h4>
+            <p className='stuff-p'><img src={logo} /> +5,000 </p>
+          </div>
+          <div className='col-3'>
+            <button className='stuff-go' type='button'>GO</button>
+          </div>
+        </div>
+        
+        <div className='row mt10 cheap-stuff' style={{width:"100%"}}>
+          <div className='col-2'>
+            <img src={youtube} style={{width:"100%"}} />
+            
+          </div>
+          <div className='col-7 stuff-text'>
+            <h4>Join our x Channel</h4>
+            <p className='stuff-p'><img src={logo} /> +5,000 </p>
+          </div>
+          <div className='col-3'>
+            <button className='stuff-claim' type='button'>CLAIM</button>
+          </div>
+        </div>
+
+        <div className='row mt10 cheap-stuff' style={{width:"100%"}}>
+          <div className='col-2'>
+            <img src={telegram} style={{width:"100%"}} />
+            
+          </div>
+          <div className='col-7 stuff-text'>
+            <h4>Join our x Channel</h4>
+            <p className='stuff-p'><img src={logo} /> +5,000 </p>
+          </div>
+          <div className='col-3'>
+            <button className='stuff-claim' type='button'>CLAIM</button>
+          </div>
+        </div>
+        
+      
+    
       </div>
-    </div>
+      </div>
   );
 };
 
